b67ecbe1164c18feed3ffa3dc9d6c610bdd4f068
Kent Hurd <khurd@makemusic.com>
2024-07-12 10:05:47 -0600

    reverted code tests

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/zsh
 
-which code
-sleep 30
-
 code --install-extension oderwat.indent-rainbow
 code --install-extension redhat.vscode-yaml
 code --install-extension GitHub.vscode-github-actions
