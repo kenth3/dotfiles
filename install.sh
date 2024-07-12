a6dafa73201010031f010868aa65e6ca16dbf485
Kent Hurd <khurd@makemusic.com>
2024-07-12 09:46:40 -0600

    trying explicit code location

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/zsh
 
-whoami
-which code
-
-code --install-extension oderwat.indent-rainbow
-code --install-extension redhat.vscode-yaml
-code --install-extension GitHub.vscode-github-actions
-
-
-
-
+/usr/local/bin/code --install-extension oderwat.indent-rainbow
+/usr/local/bin/code --install-extension redhat.vscode-yaml
+/usr/local/bin/code --install-extension GitHub.vscode-github-actions
